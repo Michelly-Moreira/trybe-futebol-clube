22e40cd51f93361aabcc4d5e2c908c6e5abf56cb
Michelly Moreira <michelly.daiana@hotmail.com>
2023-05-10 21:00:59 -0300

    criação das migrations

---
+++ app/backend/src/database/migrations/20230510224605-CreateUserTable.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING(30),
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(80),
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(20),
+      },
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
