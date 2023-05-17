f711a48c88f3c50c62d3717f3570f85b339ec286
Michelly Moreira <michelly.daiana@hotmail.com>
2023-05-17 18:20:06 -0300

    esqueleto do teste user

---
+++ app/backend/src/tests/UserTest.ts
@@ -0,0 +1,18 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { app } from '../app';
+import UserModel from '../database/models/UserModel';
+import { mockValidUser, mockInvalidUser, failureMessage, invalidDataMessage } from './mocks/UserMock';
+import Auth from '../database/utils/Auth';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testes do endpoint /login', () => {
+  it('Seu sub-teste', () => {
+    expect(false).to.be.eq(true);
+  });
+});
